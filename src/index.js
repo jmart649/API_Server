91d1e1f9ce893d2ceee6e37d5065910b2d6d6389
jmart649 <martinez.jesse84@yahoo.com>
2021-04-02 19:48:18 -0500

    Updated API Server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-console.log('Hello World')
+import './helpers/dotenv'
 
-import sum from './sum'
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import helmet from 'helmet'
 
-console.log(sum(1,3))
+import logger from './helpers/logger'
+import router from './routes'
+import { notFound, errorHandler } from './helpers/errors'
+
+const port = parseInt(process.env.PORT, 10) || 3000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(morgan(process.env.MORGAN_LOG))
+app.use(cors({ origin: process.env.ORIGIN }))
+app.use(helmet())
+
+app.use(router)
+
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(port, ()=> logger.success(`Application started at http://localhost:${process.env.PORT}`),)
